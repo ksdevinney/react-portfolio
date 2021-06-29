d3bb5458684c5f17711d0cbf9255dec8cb07e56a
ksdevinney <katie@devinney.org>
2021-06-29 15:31:01 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Projects from "./components/pages/Projects";
+import Contact from "./components/pages/Contact";
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I am an SMU undergraduate with a degree in English. I have spent the past 8 years teaching
+        elementary school, and now I am diving into web development. Computers have always been a hobby of
+        mine, so I am excited to turn my passion into my career! Currently, I am enrolled in SMU's coding
+        boot camp and excited to launch a new career soon!
+      </p>
+    </div>
+  );
+}
+
+export default About;
